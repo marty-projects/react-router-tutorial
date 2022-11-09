2fbf0de15e138f799d4fc5be25fe935825ff7797
maraty-projects <maupamaupa497@gmail.com>
2022-11-09 16:48:21 +0100

    added loader

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,22 @@ import {
   Route
 } from 'react-router-dom';
 import './index.css';
-import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import Root, {loader as rootLoader} from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
-    errorElement: <ErrorPage/>
-  },
-]);
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    children: [
+      {
+          path: "contacts/:contactId",
+          element: <Contact />,
+      },
+    ],},]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
